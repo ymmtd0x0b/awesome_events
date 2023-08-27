db9ad831f23123de603865b64351ab2d5c3a5b3c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-08-27 09:12:13 +0900

    ユーザースペックに FactoryBot を導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe User, type: :model do
   describe '.find_or_create_from_auth_hash!' do
     context 'when find a user' do
       it "returns the user's record" do
-        user = User.create(
-          provider:  'github',
-          uid:       '12345',
-          name:      'alice',
-          image_url: 'https://image.com/12345',
-        )
+        user = FactoryBot.create(:user, provider: 'github', uid: '12345')
 
         # 完璧なハッシュデータでは
         # ・ユーザーを発見できたのか
@@ -77,34 +72,11 @@ RSpec.describe User, type: :model do
   end
 
   describe 'destroy before check all events finished' do
-    before do
-      @user = User.create(
-        provider:  'github',
-        uid:       '12345',
-        name:      'alice',
-        image_url: 'https://image.com/12345',
-      )
-
-      @other_user = User.create(
-        provider:  'github',
-        uid:       '54321',
-        name:      'bob',
-        image_url: 'https://image.com/54321',
-      )
-    end
-
     context 'when user does not has publishing events' do
       it 'is successes destory record' do
         travel_to Time.local(2000, 3, 1, 0, 0) do
-          Event.create(
-            name: 'Every Rails 輪読会',
-            content: '終了イベント',
-            place: 'discord',
-            start_at: Time.now.prev_month,
-            end_at:   Time.now.prev_month + 1.hour,
-            owner: @user,
-          )
-          expect { @user.destroy }.to change(User, :count).by(-1)
+          user = FactoryBot.create(:user, :with_finished_event)
+          expect { user.destroy }.to change(User, :count).by(-1)
         end
       end
     end
@@ -112,16 +84,9 @@ RSpec.describe User, type: :model do
     context 'when user has publishing events' do
       it 'abort destroy record with returns an error message' do
         travel_to Time.local(2000, 3, 1, 0, 0) do
-          Event.create(
-            name: 'Every Rails 輪読会',
-            content: '公開中の未終了イベント',
-            place: 'discord',
-            start_at: Time.now.next_month,
-            end_at:   Time.now.next_month + 1.hour,
-            owner: @user,
-          )
-          @user.destroy
-          expect(@user.errors[:base]).to include('公開中の未終了イベントが存在します。')
+          user = FactoryBot.create(:user, :with_publishing_event)
+          user.destroy
+          expect(user.errors[:base]).to include('公開中の未終了イベントが存在します。')
         end
       end
     end
@@ -129,16 +94,8 @@ RSpec.describe User, type: :model do
     context 'when user does not has participating events' do
       it 'is successes destory record' do
         travel_to Time.local(2000, 3, 1, 0, 0) do
-          event = Event.create(
-            name: 'Every Rails 輪読会',
-            content: '終了イベント',
-            place: 'discord',
-            start_at: Time.now.prev_month,
-            end_at:   Time.now.prev_month + 1.hour,
-            owner: @other_user,
-          )
-          event.tickets.create(user: @user)
-          expect { @user.destroy }.to change(User, :count).by(-1)
+          user = FactoryBot.create(:user, :with_participating_event_that_has_finished)
+          expect { user.destroy }.to change(User, :count).by(-1)
         end
       end
     end
@@ -146,17 +103,9 @@ RSpec.describe User, type: :model do
     context 'when user has participating events' do
       it 'abort destroy record with returns an error message' do
         travel_to Time.local(2000, 3, 1, 0, 0) do
-          event = Event.create(
-            name: 'Every Rails 輪読会',
-            content: '公開中の未終了イベント',
-            place: 'discord',
-            start_at: Time.now.next_month,
-            end_at:   Time.now.next_month + 1.hour,
-            owner: @other_user,
-          )
-          event.tickets.create(user: @user)
-          @user.destroy
-          expect(@user.errors[:base]).to include('未終了の参加イベントが存在します。')
+          user = FactoryBot.create(:user, :with_participating_event)
+          user.destroy
+          expect(user.errors[:base]).to include('未終了の参加イベントが存在します。')
         end
       end
     end
