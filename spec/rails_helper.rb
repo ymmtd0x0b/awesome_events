5f5766dc122bf547ec91deb18ea0a0f72c65ac8f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-09-02 04:55:40 +0900

    shoulda-matchersの導入＆設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,11 @@ RSpec.configure do |config|
 
   config.include ActiveSupport::Testing::TimeHelpers
   config.include ActionDispatch::TestProcess::FixtureFile
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
