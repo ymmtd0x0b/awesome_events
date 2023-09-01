8c6a7d0afef93cbadcf1ef9b092ec0e88e034249
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-09-02 06:37:46 +0900

    チケットのスペックを簡潔な表記に修正

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,14 +7,5 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to be_valid
   end
 
-  it 'is valid with a comment of less then or equal 30 characters' do
-    ticket = FactoryBot.build(:ticket, comment: 'a' * 30)
-    expect(ticket).to be_valid
-  end
-
-  it 'is invalid with a comment of more then 30 characters' do
-    ticket = FactoryBot.build(:ticket, comment: 'a' * 31)
-    ticket.valid?
-    expect(ticket.errors[:comment]).to include('は30文字以内で入力してください')
-  end
+  it { is_expected.to validate_length_of(:comment).is_at_most(30) }
 end
