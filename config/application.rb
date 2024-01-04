9b1eeb7f50352a71240afb7401dce3172ce293fd
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-01-05 02:39:40 +0900

    ジェネレータがファクトリを作成できるように設定を変更

--- config/application.rb
+++ config/application.rb
@@ -29,11 +29,9 @@ module AwesomeEvents
     # Rspec
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-      g.factory_bot false
     end
   end
 end
